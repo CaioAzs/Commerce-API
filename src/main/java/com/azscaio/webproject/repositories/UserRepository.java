af5feef54277613ed21a6fb17ddc740e58481397
CaioAzs <souzascaio23cs@gmail.com>
2023-03-03 14:20:44 -0300

    Created services layer and updated the controller annotations

--- src/main/java/com/azscaio/webproject/repositories/UserRepository.java
+++ src/main/java/com/azscaio/webproject/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.azscaio.webproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.azscaio.webproject.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     
 }
---
+++ src/main/java/com/azscaio/webproject/services/UserService.java
@@ -0,0 +1,19 @@
+package com.azscaio.webproject.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.azscaio.webproject.models.User;
+import com.azscaio.webproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
