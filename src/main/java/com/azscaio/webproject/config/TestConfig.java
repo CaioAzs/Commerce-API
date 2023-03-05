b8aececbc43b6f8ad537a300c9bf0a4615b04f1f
CaioAzs <souzascaio23cs@gmail.com>
2023-03-04 21:26:42 -0300

    Order created

--- src/main/java/com/azscaio/webproject/config/TestConfig.java
+++ src/main/java/com/azscaio/webproject/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.azscaio.webproject.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.azscaio.webproject.models.Order;
 import com.azscaio.webproject.models.User;
+import com.azscaio.webproject.repositories.OrderRepository;
 import com.azscaio.webproject.repositories.UserRepository;
 
 @Configuration
@@ -17,12 +20,21 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        User u1 = new User(null, "Caio de Souza", "souzascaio23cs@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Jonas Brothers", "jonas@gmail.com", "977777777", "123456");
+
+        Order o1 = new Order(null, Instant.parse("2020-06-20T19:53:08Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2021-07-20T19:53:08Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2023-01-20T19:53:08Z"), u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
     }   
 
 }
--- src/main/java/com/azscaio/webproject/controllers/OrderController.java
+++ src/main/java/com/azscaio/webproject/controllers/OrderController.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.azscaio.webproject.models.User;
-import com.azscaio.webproject.services.UserService;
+import com.azscaio.webproject.models.Order;
+import com.azscaio.webproject.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserController {
+@RequestMapping(value = "/orders")
+public class OrderController {
 
     @Autowired
-    private UserService userService;
+    private OrderService OrderService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-        List<User> list = userService.findAll();
+    public ResponseEntity<List<Order>> findAll(){
+        List<Order> list = OrderService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User user = userService.findById(id);
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<Order> findById(@PathVariable Long id){
+        Order Order = OrderService.findById(id);
+        return ResponseEntity.ok().body(Order);
 
     }
 }
