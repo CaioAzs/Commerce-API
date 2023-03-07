4ed75dbf632bd16dc012ef4439974ec5da890814
CaioAzs <souzascaio23cs@gmail.com>
2023-03-07 13:29:05 -0300

    Error handling part2

--- src/main/java/com/azscaio/webproject/services/UserService.java
+++ src/main/java/com/azscaio/webproject/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.azscaio.webproject.models.User;
 import com.azscaio.webproject.repositories.UserRepository;
+import com.azscaio.webproject.services.exceptions.DatabaseException;
 import com.azscaio.webproject.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,13 +32,25 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);    
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update (Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try{
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }catch (RuntimeException e){
+            e.printStackTrace();
+            throw new ResourceNotFoundException(id);
+        }
     }
     
     private void updateData(User entity, User obj) {
